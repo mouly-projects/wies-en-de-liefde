dddca53cfc7463b0f77a3488d1e9b256b0683487
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-05-20 20:14:28 +0200

    Move context to index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,7 @@
-import { Head } from 'next/document'
-import type { AppProps, AppContext } from 'next/app'
-import type { SingleStore } from '../stores/single/interface'
-
-import createSingleStore, { SingleContext } from '../stores/single/store'
+import type { AppProps } from 'next/app'
 
 import '../styles/globals.scss'
-import createBandMemberStore, {
-  BandMemberContext,
-} from '../stores/band-member/store'
-import { BandMemberStore } from '../stores/band-member/interface'
 
-interface AppInterface extends AppProps {
-  singleStore: SingleStore
-  bandMemberStore: BandMemberStore
-}
-
-export default function App({
-  Component,
-  pageProps,
-  singleStore,
-  bandMemberStore,
-}: AppInterface) {
-  return (
-    <>
-      {/* <Head>
-        <title>Wies en de Liefde</title>
-      </Head> */}
-
-      <BandMemberContext.Provider value={bandMemberStore}>
-        <SingleContext.Provider value={singleStore}>
-          <Component {...pageProps} />
-        </SingleContext.Provider>
-      </BandMemberContext.Provider>
-    </>
-  )
-}
-
-App.getInitialProps = async (appContext: AppContext) => {
-  const singleStore = createSingleStore()
-  const bandMemberStore = createBandMemberStore()
-
-  return { singleStore, bandMemberStore }
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
--- utils/content/getItemSlugs.ts
+++ utils/content/getItemSlugs.ts
@@ -6,17 +6,16 @@ export default function getItemSlugs(directory: string) {
   return fs.readdirSync(directory)
 }
 
-export const getItemBySlug = (directory: string) => (
-  item: string,
-  fields = []
-) => {
-  const realSlug = item.replace(/\.md$/, '')
-  const fullPath = join(directory, `${realSlug}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
+export const getItemBySlug =
+  (directory: string) =>
+  (item: string, fields = []) => {
+    const realSlug = item.replace(/\.md$/, '')
+    const fullPath = join(directory, `${realSlug}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const { data, content } = matter(fileContents)
 
-  return data
-}
+    return data
+  }
 
 export function getAllItems(directory: string, fields = []) {
   const items = getItemSlugs(directory)
