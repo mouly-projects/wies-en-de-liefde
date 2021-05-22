3af111ae5e50f7f933ab8f6599475734bc38f183
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-05-22 13:37:08 +0200

    Add overlay on header images

--- components/modules/SiteHeader/index.tsx
+++ components/modules/SiteHeader/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import type SiteHeaderInterface from './interface'
 
 import SiteNav from '../SiteNav'
@@ -9,7 +11,11 @@ export default function SiteHeader({ theme }: SiteHeaderInterface) {
   return (
     <header className={styles['site-header']}>
       <span className={styles['logo']} data-theme={theme}>
-        <Logo />
+        <Link href="/">
+          <a className={styles['logo-link']}>
+            <Logo />
+          </a>
+        </Link>
       </span>
       <SiteNav />
     </header>
