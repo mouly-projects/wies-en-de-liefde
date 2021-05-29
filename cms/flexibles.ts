5e1539d9728fbc5f647a4a57376b9c91ec121ac7
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-05-29 11:28:49 +0200

    Add lfs and Netlify images service

---
+++ cms/flexibles.ts
@@ -0,0 +1,17 @@
+import bandMembers from '../components/flexibles/BandMembers/cms'
+import columnText from '../components/flexibles/ColumnText/cms'
+import gallery from '../components/flexibles/Gallery/cms'
+import images from '../components/flexibles/Images/cms'
+import playlist from '../components/flexibles/Playlist/cms'
+import text from '../components/flexibles/Text/cms'
+import videoEmbed from '../components/flexibles/VideoEmbed/cms'
+
+export default [
+  text,
+  columnText,
+  videoEmbed,
+  playlist,
+  bandMembers,
+  images,
+  gallery,
+]
